5766213cfac29e295bc3fe012345a9b50e1eb0f1
lem <73171983+lefpoem@users.noreply.github.com>
2021-07-16 13:53:31 +0800

    append a time counter

--- basic_Gramma/testProgressBar.py
+++ basic_Gramma/testProgressBar.py
@@ -5,10 +5,12 @@
 import time
 print("执行开始".center(16,'-'))
 scale = 10
-t = time.clock()
+# version3.9 don't support time.clock().
+t = time.process_time() # 使用一个计时器
 for i in range(scale+1):
     a,b = "**"*i,"--"*(scale-i)
     c = (i/scale)*100
+    t -= time.process_time()
     # \r光标回到本行首行
     print("\r{:>3.0f}% [{}->{}]".format(c,a,b),end='')
     time.sleep(0.1) # 增加程序暂时挂机，增加停留时间
